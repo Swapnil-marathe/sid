37f5b788edf35192cd3e54464c391e578b3d7866
sid <sidghorpade.121@gmail.com>
2022-03-12 23:01:44 +0530

    Mythread.java added

---
+++ MyThread.java
@@ -0,0 +1,18 @@
+class MyThread extends Thread {
+
+ public static void main(String [] args) {
+ MyThread t = new MyThread();
+ Thread x = new Thread(t);
+ x.start();
+ }
+
+ public void run() {
+ for( i=0;i<3;i++) {
+ System.out.print(i + "..");
+ }
+System.out.print(i + "..");
+ }
+
+ }
+ }
+}
